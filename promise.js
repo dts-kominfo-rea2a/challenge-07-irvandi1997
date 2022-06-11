845ccced8a38fea67d6cdd3a4a40e0bcce011ebb
irvandi1997 <106160897+irvandi1997@users.noreply.github.com>
2022-06-11 09:26:53 +0700

    tantangan js promise

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+    const theatersresults =  theaterIXX.concat(theaterVGC)
+    return theatersresults.map((theater) => theater.hasil === emosi).reduce((a, b) => a + b, 0);
+  }
+  catch (err) {
+    console.log(err);
+  };
+};
 
 module.exports = {
   promiseOutput,
